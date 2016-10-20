0354fbc9a0ba0e52854f710837f4a937f15db175
kit jonathan ho <j.hokit80@gmail.com>
2016-10-21 00:22:24 +0800

    required mongoose lib in app

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+//Requiring all lib from node
+//****************************
 //alowed us to set up server port and provide us with http methods ("get", "post", "put", "delete") routes
 var express = require("express");
 var ejs = require("ejs");
 var engine = require("ejs-mate");
 var morgan = require("morgan");
+var mongoose = require("mongoose");
 var app = express();
 
 app.get("/", function(req, res, next){
