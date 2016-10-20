5eae8fbc813f8d312989362aa482fc29ef5db78b
kit jonathan ho <j.hokit80@gmail.com>
2016-10-20 23:03:50 +0800

    setup public and view engine

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
+//alowed us to set up server port and provide us with http methods ("get", "post", "put", "delete") routes
 var express = require("express");
+var ejs = require("ejs");
+var engine = require("ejs-mate");
 var app = express();
 
+app.get("/", function(req, res, next){
+  res.render("home");
+})
 
-
-
+//setting up middlewares to link our libraries
+app.use(express.static(__dirname + "/public"));
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
 
 
 //server
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 7000));
 
 app.listen(app.get('port'), function() {
-  console.log('My express server is running at localhost', app.get('port'));
+  console.log('Server is running on port', app.get('port'));
 });
